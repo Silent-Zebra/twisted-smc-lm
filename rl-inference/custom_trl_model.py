52390c7b912c8b9fa91ea500989712c4a8fe6bd9
Stephen Zhao <szstephenzhao88@gmail.com>
2024-01-02 18:07:16 -0500

    do better init always

--- rl-inference/custom_trl_model.py
+++ rl-inference/custom_trl_model.py
@@ -132,10 +132,10 @@ class CustomAutoModelForCausalLMWithValueHead(PreTrainedModelWrapper):
             self.v_head.summary.weight.data.normal_(mean=0.0, std=initializer_range)
             self.v_head.summary.bias.data.zero_()
 
-            for linear_layer in self.nn_head.linear_layers:
-                in_plus_out_for_sd = linear_layer.in_features + linear_layer.out_features
-                linear_layer.weight.data.normal_(mean=0.0, std=(2. / (in_plus_out_for_sd)) ** 0.5)
-                linear_layer.bias.data.zero_()
+        for linear_layer in self.nn_head.linear_layers:
+            in_plus_out_for_sd = linear_layer.in_features + linear_layer.out_features
+            linear_layer.weight.data.normal_(mean=0.0, std=(2. / (in_plus_out_for_sd)) ** 0.5)
+            linear_layer.bias.data.zero_()
 
 
 
