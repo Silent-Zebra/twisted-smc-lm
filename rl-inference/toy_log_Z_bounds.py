008c4f44a7ac41736bc6ab17615c29b43cb89005
Stephen Zhao <szstephenzhao88@gmail.com>
2024-01-04 01:53:15 -0500

    bug fix

--- rl-inference/toy_log_Z_bounds.py
+++ rl-inference/toy_log_Z_bounds.py
@@ -4368,7 +4368,7 @@ if __name__ == "__main__":
             "one_total_kl_sample", "one_total_kl_sample_mixed_p_q",
             "one_total_kl_with_rl", "one_total_kl_with_sixo",
             "rl_p_sq", "rl_q_sq", "rl_qrsmp_sq",
-            "rl_sigma_sq", "rl_mixed_p_q_sq", "rl_p_lsq", "rl_q_lsq", "rl_q_lsq_partial_jit", "rl_q_lsq_no_grad", "rl_q_lsq_partial_jit_no_grad", "rl_qrsmp_lsq",
+            "rl_sigma_sq", "rl_mixed_p_q_sq", "rl_p_lsq", "rl_q_lsq", "rl_q_lsq_partial_jit", "rl_q_lsq_nostopgrad", "rl_q_lsq_partial_jit_nostopgrad", "rl_qrsmp_lsq",
             "rl_sigma_lsq", "rl_mixed_p_q_lsq", "rl_mixed_p_q_lsq_partial_jit", "rl_mc", "rl_mc_partial_jit",
             "sixo", "sixo_mixed_p_q", "sixo_mixed_p_q_partial_jit", "sixo_partial_jit",
             "bce", "bce_q"
