24602d4a8073fe60f4c26017ec182e1cf765202c
Stephen Zhao <szstephenzhao88@gmail.com>
2024-09-18 17:51:23 -0400

    debug

--- adv-rl/do_training.py
+++ adv-rl/do_training.py
@@ -103,6 +103,7 @@ def rl_loss(
         output_len, huggingface_model=huggingface_model)
 
     e_sigmaq_r_estimate = r_seqs.mean()
+    e_sigmaq_r_estimate = 0
 
     # Use baseline_no_grad here because we don't want the gradient for the baseline to flow through the model reward loss
     objective = ((r_seqs - e_sigmaq_r_estimate) * log_p_theta_full_seq).mean()  # Use empirical mean as estimate of the expectation
