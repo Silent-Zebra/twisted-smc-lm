20a63fe681e49054e22ddefc0f9fea2e546c2ff3
Stephen Zhao <szstephenzhao88@gmail.com>
2024-11-09 18:39:21 -0500

    save work

--- adv-rl/train_backdoored_model.py
+++ adv-rl/train_backdoored_model.py
@@ -187,7 +187,7 @@ def main():
     print(text_output, flush=True)
 
     print("Adversarial Generations")
-    generations = stochastic_transformer_sample(jax.random.PRNGKey(0), params_p, dataset_adv[0, :prompt_len + 2], output_len, 10, huggingface_model=huggingface_model)
+    generations = stochastic_transformer_sample(jax.random.PRNGKey(0), params_p, dataset_adv[0, :prompt_len + 2], output_len - 2, 10, huggingface_model=huggingface_model)
     text_output = tokenizer.batch_decode(generations)
     print(text_output, flush=True)
 
