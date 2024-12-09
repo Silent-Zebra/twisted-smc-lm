a0ae5064d3fb9ed92d38d846d368b41ba27c101d
Stephen Zhao <szstephenzhao88@gmail.com>
2024-12-09 18:14:38 -0500

    revert

--- custom_ppo_trainer.py
+++ custom_ppo_trainer.py
@@ -787,9 +787,8 @@ class PPOTrainer(BaseTrainer):
             # print(bs)
             b_inds = np.random.permutation(bs)
 
-            print(self.config.backward_batch_size)
-            print(self.config.mini_batch_size)
-            1/0
+            # print(self.config.backward_batch_size)
+            # print(self.config.mini_batch_size)
             # print(b_inds)
             for backward_batch_start in range(0, bs, self.config.backward_batch_size):
                 backward_batch_end = backward_batch_start + self.config.backward_batch_size
