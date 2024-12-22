8b814c50aefbdd09f27db8ce23a1a54f787b7563
Stephen Zhao <szstephenzhao88@gmail.com>
2024-12-22 15:23:03 -0500

    hopefully better plot saving for bounds

--- plot_utils.py
+++ plot_utils.py
@@ -136,11 +136,11 @@ def save_logZ_bounds_plot(
     if proposal_is_p:
         figname = f"{save_dir}/fig_pproposal_logZ_bounds_by_samples_over_time_epoch{epoch_starting_from_0}.pdf"
         # ckpt_name = f"logZ_bounds_pproposal_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}_seed{seed}_{twist_learn_type}_nsamples"
-        ckpt_name = f"logZ_bounds_pproposal_{load_prefix_str}_nsamples"
+        ckpt_name = f"logZ_bounds_pproposal_{load_prefix_str}_nsamples{n_samples_for_plots[0]}_{n_samples_for_plots[1]}_"
     else:
         figname = f"{save_dir}/fig_twistproposal_logZ_bounds_by_samples_over_time_epoch{epoch_starting_from_0}.pdf"
         # ckpt_name = f"logZ_bounds_twistproposal_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}_seed{seed}_{twist_learn_type}_nsamples"
-        ckpt_name = f"logZ_bounds_twistproposal_{load_prefix_str}_nsamples"
+        ckpt_name = f"logZ_bounds_twistproposal_{load_prefix_str}_nsamples{n_samples_for_plots[0]}_{n_samples_for_plots[1]}_"
 
     plt.savefig(figname)
 
