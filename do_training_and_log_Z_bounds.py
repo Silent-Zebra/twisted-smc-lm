8d66fa5cfdabfa868c5d588a0e1f63819e75fc05
Stephen Zhao <szstephenzhao88@gmail.com>
2025-01-02 17:03:45 -0500

    debug

--- do_training_and_log_Z_bounds.py
+++ do_training_and_log_Z_bounds.py
@@ -2453,8 +2453,17 @@ def main():
             #     log_psi = final_activations[:, prompt.shape[-1]:] @ params_twist[
             #         'value_head'].squeeze()
             #     print("VALUE ON TRUE POSTERIOR SAMPLES FOR EVERY PARTIAL SEQUENCE")
-            #     print(log_psi) # TODO DEBUG ONLY REMOVE LATER
+            #     print(log_psi) # DEBUG ONLY
             #     1/0
+
+
+            # TODO DEBUG ONLY REMOVE LATER
+            get_p_logits_and_log_psi_all_vocab(
+                true_posterior_samples_by_token, params_p, params_twist,
+                condition_twist_on_tokens=None, huggingface_model=huggingface_model,
+                params_proposal=None, prompt_len=prompt.shape[-1]
+            )
+            1/0
 
             # ----- DO plotting and inspection of test info before the twist updates -----
             if (not args.no_test_info) and ((epoch + 1) % args.print_every == 0):
