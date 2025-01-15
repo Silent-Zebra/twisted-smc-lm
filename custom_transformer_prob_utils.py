193d98712c76130d2347dda23e1f70ec57a15dbc
Stephen Zhao <szstephenzhao88@gmail.com>
2025-01-14 21:19:56 -0500

    attempt load PPO policy

--- custom_transformer_prob_utils.py
+++ custom_transformer_prob_utils.py
@@ -1073,12 +1073,25 @@ def smc_scan_iter_final(rng_key, full_seq, log_w_t, log_gamma_1_to_t_eval, log_p
     params_twist_to_use = params_twist
     if proposal_is_p:
         params_twist_to_use = None
-    rng_key, full_seq, normalized_log_q_t, log_p_eval_of_new_seqs, log_psi_eval_of_new_seqs = get_proposal_q_sample(
-        rng_key, full_seq, params_p, params_twist_to_use, prompt_len, t,
-        condition_twist_on_tokens,  proposal_is_p=proposal_is_p,
-        huggingface_model=huggingface_model, true_posterior_sample=true_posterior_sample,
-        tempered_twist=tempered_twist, beta_prop=beta_prop, params_proposal=params_proposal
-    )
+
+    if isinstance(params_proposal, HashableDict):
+        rng_key, full_seq, normalized_log_q_t, log_p_eval_of_new_seqs, log_psi_eval_of_new_seqs = get_proposal_q_sample_nojit(
+            rng_key, full_seq, params_p, params_twist_to_use, prompt_len, t,
+            condition_twist_on_tokens, proposal_is_p=proposal_is_p,
+            huggingface_model=huggingface_model,
+            true_posterior_sample=true_posterior_sample,
+            tempered_twist=tempered_twist, beta_prop=beta_prop,
+            params_proposal=params_proposal
+        )
+    else:
+        rng_key, full_seq, normalized_log_q_t, log_p_eval_of_new_seqs, log_psi_eval_of_new_seqs = get_proposal_q_sample(
+            rng_key, full_seq, params_p, params_twist_to_use, prompt_len, t,
+            condition_twist_on_tokens, proposal_is_p=proposal_is_p,
+            huggingface_model=huggingface_model,
+            true_posterior_sample=true_posterior_sample,
+            tempered_twist=tempered_twist, beta_prop=beta_prop,
+            params_proposal=params_proposal
+        )
 
     log_p_theta_t_eval = log_p_eval_of_new_seqs
 
