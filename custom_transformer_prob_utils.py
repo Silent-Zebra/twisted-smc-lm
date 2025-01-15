16e28569f4b1722014cfa84696ab206be75072f1
Stephen Zhao <szstephenzhao88@gmail.com>
2025-01-14 23:33:58 -0500

    attempt load PPO policy

--- custom_transformer_prob_utils.py
+++ custom_transformer_prob_utils.py
@@ -382,20 +382,21 @@ def get_proposal_q_sample_nojit(rng_key, full_seq, params_p, params_twist, promp
         # print(normalized_log_q_t.shape)
 
 
+        if params_twist is None:
+            log_psi = None
+            p_logits = get_transformer_p_logits(params_p,
+                                                full_seq[:, :prompt_len + t],
+                                                huggingface_model=huggingface_model)
+            log_p = jax.nn.log_softmax(p_logits[:, prompt_len + t - 1, :])
 
-        log_p, log_psi = get_log_p_plus_log_psi_t(full_seq, params_p,
-                                                  params_twist, prompt_len, t,
-                                                  condition_twist_on_tokens,
-                                                  huggingface_model=huggingface_model)
+        else:
+            log_p, log_psi = get_log_p_plus_log_psi_t(full_seq, params_p,
+                                                      params_twist, prompt_len, t,
+                                                      condition_twist_on_tokens,
+                                                      huggingface_model=huggingface_model)
 
 
-        # p_logits = get_transformer_p_logits(params_p,
-        #                                     full_seq[:, :prompt_len + t],
-        #                                     huggingface_model=huggingface_model)
-        # log_p2 = jax.nn.log_softmax(p_logits[:, prompt_len + t - 1, :])
-        #
-        # print(log_p)
-        # print(log_p2)
+
 
         if tempered_twist:
             log_psi = beta_prop * log_psi
