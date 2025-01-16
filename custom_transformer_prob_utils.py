df12d17e7579908efa14e325523ee06c183b9099
Stephen Zhao <szstephenzhao88@gmail.com>
2025-01-15 21:56:29 -0500

    try faster/everything jax for loading OpenRLHF checkpoint

--- custom_transformer_prob_utils.py
+++ custom_transformer_prob_utils.py
@@ -378,7 +378,7 @@ def get_proposal_q_sample_nojit(rng_key, full_seq, params_p, params_twist, promp
         model_output = params_proposal['model'](
             input_ids=full_seq)
         print(model_output)
-        q_logits = model_output
+        q_logits = model_output.logits
         # 1 / 0
 
         log_q_all_tokens = jax.nn.log_softmax(q_logits, axis=-1)
@@ -570,7 +570,7 @@ def evaluate_normalized_log_q_1_to_t_nojit(
         model_output = params_proposal['model'](
             input_ids=full_seq)
         print(model_output)
-        q_logits = model_output
+        q_logits = model_output.logits
 
         normalized_log_q_t_all_vocab = jax.nn.log_softmax(q_logits, axis=-1)[:, prompt_len - 1: -1]
         # print(normalized_log_q_t_all_vocab.shape)
