980fb8dd1ff0797003ff5e7ecd0d3c763e7c5de0
Stephen Zhao <szstephenzhao88@gmail.com>
2025-01-16 22:24:31 -0500

    debug/understand only

--- custom_transformer_prob_utils.py
+++ custom_transformer_prob_utils.py
@@ -1368,17 +1368,18 @@ def smc_debug(rng_key, prompt, params_p, params_twist, log_true_final_twist, out
         )
     log_z_hat_t_record.append(log_z_hat_t)
 
-    print("full_seq_list")
-    print(full_seq_list) # TODO later consider passing in a tokenizer to decode this too.
-
-    print("log_z_hat_t_record")
-    print(log_z_hat_t_record)
-
-    print("log_w_t_list")
-    print(log_w_t_list)
-
-    print("log_psi_t_eval_list")
-    print(log_psi_t_eval_list)
+    # Debug only
+    # print("full_seq_list")
+    # print(full_seq_list) # TODO later consider passing in (or hardcoding) a tokenizer to decode this too.
+    #
+    # print("log_z_hat_t_record")
+    # print(log_z_hat_t_record)
+    #
+    # print("log_w_t_list")
+    # print(log_w_t_list)
+    #
+    # print("log_psi_t_eval_list")
+    # print(log_psi_t_eval_list)
 
     # print(time.time() - start)
     # start = time.time()
